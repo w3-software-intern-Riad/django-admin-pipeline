f1f835996b66bd941775800fe1e62557c608a5f0
w3-software-intern-Riad <wengineerssoftwareengineer@gmail.com>
2024-08-20 12:30:49 +0600

    change

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from polls.models import HotelInformation, Images, Amenities, Location
 import os
 
-
 class ImagesInline(admin.TabularInline):
     model = Images
     extra = 1
@@ -11,15 +10,14 @@ class ImagesInline(admin.TabularInline):
     exclude = ('updateDate',)
     can_delete = True
 
-
 class HotelInformationAdmin(admin.ModelAdmin):
-
     inlines = [ImagesInline]
     list_display = ('title', 'description', 'get_locations',
                     'get_amenities', 'get_images', 'createDate', 'updateDate')
     exclude = ('updateDate',)
     search_fields = ['title']
     filter_horizontal = ('locations', 'amenities')
+    list_filter = ('createDate', 'updateDate', 'locations', 'amenities')  # Added filter
 
     def get_locations(self, obj):
         return ", ".join([location.name for location in obj.locations.all()]) if obj.locations.exists() else "No Locations"
@@ -35,7 +33,7 @@ class HotelInformationAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
-
+        
         # Handle Location
         location_name = form.cleaned_data.get('location_name')
         if location_name:
@@ -51,11 +49,11 @@ class HotelInformationAdmin(admin.ModelAdmin):
         for obj in queryset:
             obj.delete()  # This will trigger the delete method in the model
 
-
 class ImageInformationAdmin(admin.ModelAdmin):
     list_display = ('image_name', 'hotel', 'createDate', 'updateDate')
     search_fields = ['image']
     exclude = ('updateDate',)
+    list_filter = ('createDate', 'updateDate', 'hotel')  # Added filter
 
     def image_name(self, obj):
         return os.path.basename(obj.image.name) if obj.image else "No image"
@@ -69,27 +67,26 @@ class ImageInformationAdmin(admin.ModelAdmin):
         for obj in queryset:
             obj.delete()  # This will trigger the delete method in the model
 
-
 class LocationInformationAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'get_hotels', 'latitude',
                     'longitude', 'createDate', 'updateDate')
     search_fields = ['name', 'hotels__title']
     exclude = ('updateDate',)
+    list_filter = ('type', 'createDate', 'updateDate')  # Added filter
 
     def get_hotels(self, obj):
         return ", ".join([hotel.title for hotel in obj.hotels.all()]) if obj.hotels.exists() else "No Hotels"
     get_hotels.short_description = 'Hotels'
-
 
 class AmenitiesInformationAdmin(admin.ModelAdmin):
     list_display = ('name', 'get_hotels', 'createDate', 'updateDate')
     search_fields = ['name', 'hotels__title']
     exclude = ('updateDate',)
+    list_filter = ('createDate', 'updateDate')  # Added filter
 
     def get_hotels(self, obj):
         return ", ".join([hotel.title for hotel in obj.hotels.all()]) if obj.hotels.exists() else "No Hotels"
     get_hotels.short_description = 'Hotels'
-
 
 # Register the models with the customized admin classes
 admin.site.register(HotelInformation, HotelInformationAdmin)
